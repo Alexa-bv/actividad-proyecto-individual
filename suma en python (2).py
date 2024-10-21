60f6af19176e8d32ea44e88d28f32654c1213b16
ParZibal01 <talexandra185@gmail.com>
2024-10-20 21:17:17 -0500

    docs:se agrego el archivo

---
+++ suma en python (2).py
@@ -0,0 +1,13 @@
+//resta_de_dos_numeros
+
+
+# Pide al usuario que ingrese dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Calcula la resta
+resta = num1 - num2
+
+# Muestra el resultado
+print(f"La resta de {num1} y {num2} es: {SUMA}")
+
